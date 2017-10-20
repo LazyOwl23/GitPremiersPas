07b24e1a3004707f8e8b4f366de689534a8ee333
GIT <git@LD140-55-06265.local>
2017-10-20 09:37:28 -0400

    ajout de fonction "afficher"

--- GitPremiersPas/ViewController.swift
+++ GitPremiersPas/ViewController.swift
@@ -13,13 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        afficher("bonjour git", fois: 5)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //MARK:- Mes fonctions
+    //TODO: A Corriger nb * -1
+    func afficher(_ msg:String, fois=Int = 1){
+        for i in 0...fois{
+            print(i, msg)
+        }//for
+    }//afficher
+    
+    
 }
 
